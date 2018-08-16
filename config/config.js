5c63816389d62322fcee1061fed18af42d1df4ff
lixiangbiao <lixiangbiao@tzt.cn>
2018-08-16 13:48:16 +0800

    node后台
    node与mysql的连接,操作数据库等

---
+++ config/config.js
@@ -0,0 +1,10 @@
+var confi = {
+    mysql:{
+        host:'127.0.0.1',
+        user:'root',
+        password:'123456',
+        database:'blog',
+        port:'3306'
+    }
+}
+module.exports = confi;
